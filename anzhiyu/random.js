d1363bd807c2970a7f2f66d2a235f733d5da7285
“zhangzhanglaila” <2736201404@qq.com>
2025-10-01 01:22:21 +0800

    Site updated: 2025-10-01 01:22:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/30/科目一/","2025/08/20/科目二/","2025/09/11/hello-world/","2025/04/20/网络工程与科学导论/"];function toRandomPost(){
+var posts=["2025/08/20/科目二/","2025/09/11/hello-world/","2025/07/30/科目一/","2025/04/20/网络工程与科学导论/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
